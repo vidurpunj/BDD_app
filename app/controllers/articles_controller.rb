8de9c1aaa3ada0edfc575ad4d081ffdf06b3fe13
vidur.tukatech <vidurp@tukatech.com>
2020-08-25 14:03:00 +0530

    Master udpate

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,6 +25,7 @@ class ArticlesController < ApplicationController
   # POST /articles
   # POST /articles.json
   def create
+    @article = Article.new(article_params)
 
     respond_to do |format|
       if @article.save
